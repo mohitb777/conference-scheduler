197e77d8a9a4310d1f0570c5d3e9f946217e590b
mohitb777 <mohitbari8@gmail.com>
2025-01-24 19:39:18 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
-  },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://conference-scheduler-bay.vercel.app',
+       target: 'https://conference-scheduler-ns0z4zt2b-mohits-projects-a2c7dc06.vercel.app.app',
         changeOrigin: true,
         secure: false
       }
