f9bf01d0d4fd4b1743b897eb8ded617c9be11542
mohitb777 <mohitbari8@gmail.com>
2025-01-26 22:54:28 +0530

    change search button added

--- backend/constants/scheduleConstants.js
+++ backend/constants/scheduleConstants.js
@@ -27,14 +27,14 @@ const sessionTimeSlotMapping = {
 const sessionVenueMapping = {
   'Session 1': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 337',
   'Session 2': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 343',
-  'Session 3': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 285',
+  'Session 3': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 340',
   'Session 4': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 339',
-  'Session 5': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 341',
+  'Session 5': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 342',
   'Session 6': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 337',
   'Session 7': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 343',
-  'Session 8': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 285',
+  'Session 8': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 340',
   'Session 9': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 339',
-  'Session 10': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 341'
+  'Session 10': 'AITR Building Block 3 CSIT Dept. 3rd Floor Lab No. 342'
 };
 
 module.exports = {
